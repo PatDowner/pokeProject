8bebb8af66f1c8d0c5b0596ca5e7cb1407358d1c
Pat Downer <patricia.downer@gmail.com>
2020-08-03 16:56:15 -0700

    successfully pulls all cards for searched pokemon!

--- assets/app.js
+++ assets/app.js
@@ -3,59 +3,47 @@
 // }
 
 
-axios.get(`https://api.pokemontcg.io/v1/cards/name`)
-  .then(res => {
-    // console.log(res.data)
-    document.getElementById('search').addEventListener('click', event => {
-      event.preventDefault()
+document.getElementById('search').addEventListener('click', event => {
+  event.preventDefault()
 
-      // start at index 0 so we can go through the whole index later
-      let currentIndex = 0
+  // console.log the searched name
+  console.log(document.getElementById('nameInput').value)
+  // console.log(res.data.cards[0].name)
 
-      // console.log(currentIndex)
+  // name typed into search field
+  let pokeName = document.getElementById('nameInput').value
 
-      // console.log the searched name
-      console.log(document.getElementById('nameInput').value)
-      // console.log(res.data.cards[0].name)
+  axios.get(`https://api.pokemontcg.io/v1/cards?name=${pokeName}`)
+    .then(res => {
 
-      // name typed into search field
-      let pokeName = document.getElementById('nameInput').value
-      let pokeNameIndex = ''
-      // // Run a loop to find which item in teh array matches that name
-      // for (let i = 0; i < res.data.cards.length; i++) {
+      console.log(res.data.cards)
 
-      //   // finds index position of that name
-      //   if (pokeName === res.data.cards[i].name) {
-      //     pokeNameIndex = currentIndex
+      // // test
+      // console.log(res.data.cards[5].name)
 
-      //   } else {
-      //     // console.log('else ' + pokeName)
-      //     // console.log([i])
-      //     currentIndex++
-      //   }
-      //   // end of loop
-      // }
-      // console.log(pokeNameIndex)
 
-      // Run loop to fetch all the cards for pokeName
-      for (let i = 0; i < res.data.cards.length; i++)
-        if (pokeName === res.data.cards[i].name) {
-          console.log(res.data.cards[i].name)
-          console.log(res.data.cards[i].imageUrlHiRes)
-        } else {
-          console.log('oops')
-        }
-      // console.log(imgLink)
+      for (let i = 0; i < res.data.cards.length; i++) {
+        let pokeCard = document.createElement('div')
+        let imgLink = res.data.cards[i].imageUrlHiRes
+        pokeCard.innerHTML = `<img src="${imgLink}" class="pokeCard">`
+        document.getElementById('cardDisplay').prepend(pokeCard)
+
+
+      }
+
+
       // let pokeCard = document.createElement('img')
       // pokeCard.innerHTML = `<img src="${imgLink}" class="pokeCard">`
       // console.log(`https://api.pokemontcg.io/v1/cards?name=${pokeName}`)
       // document.getElementById('cardDisplay').append(pokeCard)
+      // end loop
+      // }
+      //   // end of button click listener
+      // })
 
-      // end of button click listener
+      // end of .then
     })
-
-    // end of .then
-  })
-  .catch(err => {
-    console.log(err)
-  })
+    .catch(err => {
+      console.log(err)
+    })
+})
