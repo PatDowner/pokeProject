ea812355af6c41a89f829fa6b02f9ecbfd30f2f9
Devin Castro <dcrocker21@gmail.com>
2020-08-04 14:34:31 -0700

    updated for day 2

--- assets/data.js
+++ assets/data.js
@@ -11,6 +11,7 @@ $('#searchPokemon').click(() => {
 
 
       $('#pokemonPicture').html(`
+      <h4 id="pokeName">${$('#pokemon').val()}</h4>
         <img src="${result.sprites.front_default}" alt="Pokemon Front Image">
       `)
       
@@ -45,41 +46,6 @@ $('#searchPokemon').click(() => {
         </p>
       `)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      $('#results').html(`
-        <img src="${result.sprites.front_default}" alt="Pokemon Front Image">
-        <p>Type: <button id="typeColor" class="btn waves-effect waves-light">${result.types[0].type.name}</button> </p>
-        <p>height: ${result.height}ft </p>
-        <p>Weight: ${result.weight}lbs </p>
-        `)
-
-
-      $('#stats').html(`
-                  <p>Stats: 
-                  <br>
-                  ${result.stats[0].stat.name} Base: ${result.stats[0].base_stat} <br>
-                  ${result.stats[1].stat.name} Base: ${result.stats[1].base_stat} <br> 
-                  ${result.stats[2].stat.name} Base: ${result.stats[2].base_stat} <br>
-                  ${result.stats[3].stat.name} Base: ${result.stats[3].base_stat} <br>
-                  ${result.stats[4].stat.name} Base: ${result.stats[4].base_stat} <br>
-                  ${result.stats[5].stat.name} Base: ${result.stats[5].base_stat} <br>
-                  </p>
-                  `)
-                  
 
 
 
