c3fed64db070b17862772cec31e97cca4aaa1a6a
Devin Castro <dcrocker21@gmail.com>
2020-08-04 11:42:10 -0700

    fully functional data display

--- assets/data.js
+++ assets/data.js
@@ -10,6 +10,54 @@ $('#searchPokemon').click(() => {
       console.log(result)
 
 
+      $('#pokemonPicture').html(`
+        <img src="${result.sprites.front_default}" alt="Pokemon Front Image">
+      `)
+      
+      
+
+
+      
+      $('#pokemonInfo').html(`
+      <p>Type: <button id="typeColor" class="btn waves-effect waves-light">${result.types[0].type.name}</button> </p>
+      <p>Height: ${result.height}ft </p>
+      <p>Weight: ${result.weight}lbs </p>
+      `)
+      
+      $('#typeColor').addClass(`${result.types[0].type.name}`)
+
+
+
+      // work on this later!!!
+      // $('#test').removeClass(`yellow ${result.types[0].type.name}`)
+      // $('#test').addClass(`${result.types[0].type.name}`)
+      
+
+      $('#pokemonStats').html(`
+        <p>Stats:
+        <br>
+        Base: ${result.stats[0].stat.name}: ${result.stats[0].base_stat} <br>
+        Base: ${result.stats[1].stat.name}: ${result.stats[1].base_stat} <br> 
+        Base: ${result.stats[2].stat.name}: ${result.stats[2].base_stat} <br>
+        Base: ${result.stats[3].stat.name}: ${result.stats[3].base_stat} <br>
+        Base: ${result.stats[4].stat.name}: ${result.stats[4].base_stat} <br>
+        Base: ${result.stats[5].stat.name}: ${result.stats[5].base_stat} <br>
+        </p>
+      `)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
       $('#results').html(`
@@ -18,12 +66,6 @@ $('#searchPokemon').click(() => {
         <p>height: ${result.height}ft </p>
         <p>Weight: ${result.weight}lbs </p>
         `)
-
-      $('#typeColor').addClass(`${result.types[0].type.name}`)
-      
-
-
-
 
 
       $('#stats').html(`
@@ -49,7 +91,7 @@ $('#searchPokemon').click(() => {
             console.log(res)
             console.log(res.effect_entries[1].effect)
          
-            $('#abilities').append(`<p>${res.name}: ${res.effect_entries[1].effect}</p>`)
+            $('#abilities').append(`<p>${res.name}: ${res.effect_entries[1].effect}</p><br>`)
 
           })
           .catch(err => {
