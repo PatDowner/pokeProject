a3c00cb5716a3d90eda3aa823f341de724335adf
Pat Downer <patricia.downer@gmail.com>
2020-08-06 11:16:52 -0700

    added end game, high score, & timer functionality

--- assets/trivia.js
+++ assets/trivia.js
@@ -1,3 +1,28 @@
+// Set high score to zero
+let highScore = 0
+
+// user name indicates no high score until player has logged an initial high score
+let userName = 'No current high score'
+
+// recalls last high score if user has visited page before or sets up an empty array ready to accept a new high score
+let pokeScoreLog = JSON.parse(localStorage.getItem('pokeScoreLog')) || []
+
+// if there's data in localStorage for pokeScoreLog, use that to set the highScore and user
+for (let i = 0; i < pokeScoreLog.length; i++) {
+  highScore = pokeScoreLog[i].highScore
+  userName = pokeScoreLog[i].userName
+}
+
+// Display current highScore in HTML
+document.getElementById('highScore').innerHTML = `
+High Score:&nbsp;${highScore}<br>
+User: ${userName}
+`
+
+
+
+
+
 // setting up some arrays variables to populate later
 let pick4 = []
 let random
@@ -5,6 +30,9 @@ let answers = []
 let answer
 let score = 0
 document.getElementById('score').innerHTML = `Score: ${score}`
+
+// Set timer start value
+let seconds = 10
 
 // get 4 new pokemon (and a correct one) to pull for a question
 const newValues = () => {
@@ -108,7 +136,75 @@ const questionImage = (y) => {
 }
 
 
+const endGame = () => {
+  document.getElementById('questionsDiv').classList.add('hide')
+  document.getElementById('answersDiv').classList.add('hide')
+  document.getElementById('finished').classList.remove('hide')
 
+  if (score > highScore) {
+    // set user's score to be the current highScore
+    highScore = score
+
+    // display text congratulating user
+    document.getElementById('finalScore').innerHTML = `
+      <p>Congratulations! You have achieved a new high score!!</p>
+      <p>Your Score: ${highScore}</p>
+      `
+
+    // unhide finalScore element
+    document.getElementById('finalScore').classList.remove('hide')
+
+    document.getElementById('userSet').classList.remove('hide')
+
+    document.getElementById('save').addEventListener('click', event => {
+      event.preventDefault()
+
+      // take user's input and store as userName
+      userName = document.getElementById('initials').value
+
+      // scoreObj array that will be pushed onto pokeScoreLog
+      let scoreObj = {
+        highScore: highScore,
+        userName: userName
+      }
+
+      // push the scoreObj to pokeScoreLog
+      pokeScoreLog.push(scoreObj)
+
+      // pushing pokeScoreLog to localStorage making userName and highScore available for reference on refresh
+      localStorage.setItem('pokeScoreLog', JSON.stringify(pokeScoreLog))
+
+      // display confirmation that new highScore is saved
+      document.getElementById('input').innerHTML = `
+      <p class="text-success">New high score saved!<br>
+      High Score: ${highScore}<br>
+      User: ${userName}</p>
+      `
+      document.getElementById('save').classList.add('hide')
+
+      // Display current high score in HTML
+      document.getElementById('highScore').innerHTML = `
+        High Score: ${highScore}<br>
+        User: ${userName}
+        `
+      // show start over button
+      document.getElementById('startOver').classList.remove('hide')
+    })
+
+  } else {
+    // otherwise, just display final score
+    document.getElementById('finalScore').innerHTML = `
+    <p>Your score: ${score}</p>
+    <p>High score: ${highScore}<br>
+    (by user: ${userName})</p>
+    `
+
+    // unhide finalScore element and start over button
+    document.getElementById('finalScore').classList.remove('hide')
+    document.getElementById('startOver').classList.remove('hide')
+
+  }
+}
 
 
 
@@ -125,6 +221,30 @@ document.getElementById('startBtn').addEventListener('click', event => {
   questionImage(random)
   // console.log(pick4)
   // console.log(random)
+
+  // start a timer that runs over intervals of 1 second (1000 ms)
+  timer = setInterval(() => {
+    // for each interval, subtract a second from the timer
+    seconds--
+
+    // and update that time in the HTML page
+    document.getElementById('time').textContent = seconds
+
+    // once the seconds are less than or equal to zero...
+    if (seconds <= 0) {
+      // clear the timer
+      clearInterval(timer)
+      // and run the endGame() function
+      endGame()
+    }
+  }, 1000)
+
+
+
+})
+
+document.getElementById('startOver').addEventListener('click', event => {
+  location.reload()
 })
 
 // global event listener
